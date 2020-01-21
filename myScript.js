0f56d6b794887350e9d078dc4e3de0f8154a7d85
Joe <47640432+Juicebox343@users.noreply.github.com>
2020-01-21 14:42:41 -0800

    Initial commit

---
+++ myScript.js
@@ -0,0 +1,42 @@
+const COLOR_ARRAY = ["#ff4d4d", "#ff8c00", "#f6d300", "#009959", "#668cff", "#b300b3"];
+const MY_NAME = "JOE HEISER";
+const NAME_ARRAY = MY_NAME.split("");
+
+let header = document.getElementById('header');
+let og_confetti = document.querySelector('.confetti'); 
+
+let newArray = COLOR_ARRAY.slice();
+
+NAME_ARRAY.forEach(function(letter){
+	let letterSpan = document.createElement("span"); //Create the DOM elements
+	letterSpan.textContent = letter;
+	letterSpan.setAttribute('class', 'animate-me');
+	document.getElementById("my-name").appendChild(letterSpan);
+	if (letter != " "){ 						//Don't assign colors to blank spans
+		letterSpan.style.color =  newArray[0];	//assign color
+		newArray.push(newArray[0]); 			//push and shift array to allow for infinite loop
+		newArray.shift();
+	}
+});
+
+COLOR_ARRAY.forEach(function(element){
+	let boxDiv = document.createElement("div"); //Create the DOM elements
+	boxDiv.setAttribute('class', 'array-boxes');
+	document.getElementById("array-methods").appendChild(boxDiv);
+	boxDiv.style.backgroundColor = element;
+});
+
+
+for (i = 1; i < 150; i++){
+	let confetti = document.createElement("div");
+	confetti.setAttribute('class', 'confetti');
+	confetti.style.left = Math.floor(Math.random() * header.offsetWidth-10) + "px";
+	confetti.style.top = Math.floor(Math.random() * header.offsetHeight-10) + "px";
+	confetti.style.transform = "rotate(" + Math.floor(Math.random() * 360) + "deg)";
+	randomColor(confetti);
+	header.appendChild(confetti);
+}
+
+function randomColor(item){
+	item.style.backgroundColor = COLOR_ARRAY[Math.floor(Math.random() * COLOR_ARRAY.length)];
+}
